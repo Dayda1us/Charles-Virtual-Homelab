2f5405fbc364afce0692cd685ca8fad53a15d8f7
Dayda1us <145280599+Dayda1us@users.noreply.github.com>
2023-10-17 15:33:56 -0700

    Add files via upload

--- PowerShell/Join-NCRDomain.ps1
+++ PowerShell/Join-NCRDomain.ps1
@@ -5,22 +5,46 @@ function Join-NCRDomain {
     )
 
     BEGIN {
-        $RandomNumber = Get-Random -Minimum 10000000 -Maximum 99999999
+        $RandomNumber = Get-Random -Minimum 1000 -Maximum 9999
         if ($null -ne $computerName) {
+            $LocInitial = @('SS','HO','MC')
+
             [ValidateSet('Desktop','Laptop')]
             $ComputerType = Read-Host "Are $computerName Desktop or Laptop?"
+
             if ($ComputerType -eq 'Desktop') {
-                foreach ($computer in $computerName) {
-                    Rename-Computer -ComputerName $computer -NewName "DESKTOP-$RandomNumber" -WhatIf
+                [ValidateSet('ShadySands','HeliosOne','McCarren')]
+                $Location = Read-Host "Specify the location for these computers [ShadySands, HeliosOne, McCarren]"
+
+                try {
+                    switch ($Location) {
+                        {$Location -eq 'ShadySands'} {
+                            foreach ($computer in $computerName) {
+                                Rename-Computer -ComputerName $computer -NewName "$($LocInitial[0])-DT$RandomNumber"
+                            }
+                        }
+                        {$Location -eq 'HeliosOne'} {
+                            foreach ($computer in $computerName) {
+                                Rename-Computer -ComputerName $computer -NewName "$($LocInitial[1])-DT$RandomNumber" -WhatIf
+                            }
+                        }
+                        {$Location -eq 'McCarren'} {
+                            foreach ($computer in $computerName) {
+                                Rename-Computer -ComputerName $computer -NewName "$($LocInitial[2])-DT$RandomNumber" -WhatIf
+                            }
+                        }
+                    } #switch Location
+                } catch [Microsoft.PowerShell.Commands.RenameComputerCommand] {
+                    Write-Warning "$computer does not exist."
                 }
             } elseif ($ComputerType -eq 'Laptop') {
                 foreach ($computer in $computerName) {
                     Rename-Computer -ComputerName $computer -NewName "LAPTOP-$RandomNumber" -WhatIf
                 }
-            }
-        }
+            } #if Desktop/Laptop
+        } #if $computername
 
-    }
+    } #BEGIN
     PROCESS {
 
     }
